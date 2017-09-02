0e875955e8d0b5aeb2daafc0ca59d22ea7806404
PrajwalRavi <prjwl132@gmail.com>
2017-09-02 18:00:32 +0530

    Started working on downloading by name.

---
+++ name.py
@@ -0,0 +1,46 @@
+from collections import __main__
+from tkinter import *
+from tkinter import filedialog
+from pytube import YouTube
+import requests
+from bs4 import BeautifulSoup
+
+import ytdownloader
+
+class GetByName:
+
+    def __init__(self,root, name, location):
+
+        self.res=[]
+        self.master=root
+        self.name= name
+        self.dow_loc= location
+
+
+    def get_vids:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main:
+
+    root=Tk()
+    obj= GetByName(root)
+    obj.get_vids()
+    root.mainloop()
+
--- ytdownloader.py
+++ ytdownloader.py
@@ -13,26 +13,33 @@ class Gui:
 		self.master=root
 		self.dow_loc=StringVar()
 		self.dow_loc.set("/home/prajwal/Desktop/Downloads")
+		self.mode="Name"
+		self.vid= None
 
-	def get_method(self):
+	def get_mode(self):
 
 		master=self.master
+		self.vid = StringVar()
 		option_list = ("Name", "URL")
-		selection = StringVar()
-		selection.set("Name")
+		self.mode = StringVar()
+		self.mode.set("Name")
 
-		label1 = Label(master, text="Search by:")
-		option = OptionMenu(master, selection, *option_list)
-		okay = Button(master, text="Okay", command=master.destroy)
+		label1 = Label(master, text="Search by: ")
+		option = OptionMenu(master, self.mode, *option_list)
+		okay = Button(master, text="Okay", command=master.destroy )
 		label2 = Label(master, text="Download location:")
-		self.loc_disp= Label(master, bg="white", textvariable=self.dow_loc)
+		label3 = Label(master, text="Video name / URL: ")
+		tex_input = Entry(master, textvariable=self.vid)
+		self.loc_disp= Label(master, bg="white", textvariable=self.dow_loc, padx=20)
 		browse = Button(master, text="Browse", command=self.get_loc)
 
 		option.grid(row=0, column=1, sticky="W")
 		label1.grid(row=0, column=0, sticky=E)
 		label2.grid(row=1, column=0)
 		browse.grid(row=1, column=2)
+		label3.grid(row=2)
 		self.loc_disp.grid(row=1, column=1)
+		tex_input.grid(row=2, column=1, sticky=W)
 		okay.grid(columnspan=3)
 
 
@@ -41,39 +48,13 @@ class Gui:
 		d = filedialog.askdirectory()
 		self.dow_loc.set(d)
 
-	def get_info(self,root):
 
-		master=root
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def main_root_def():
+def main():
 
 	root1=Tk()
 	gui_obj = Gui(root1)
-	gui_obj.get_method()
+	gui_obj.get_mode()
 	root1.mainloop()
 
-	root2=Tk()
-	gui_obj.get_info(root2)
-
-	root2.mainloop()
-
-
-
-
-
 if __name__=="__main__":
-	main_root_def()
+	main()
